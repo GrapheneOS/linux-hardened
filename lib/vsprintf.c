b61e5bf15eba96831cd496894b9b3f4c7eeca144
Daniel Micay <danielmicay@gmail.com>
2017-05-03 12:06:14 -0400

    set kptr_restrict=2 by default
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1591,7 +1591,7 @@ char *device_node_string(char *buf, char *end, struct device_node *dn,
 	return widen_string(buf, buf - buf_start, end, spec);
 }
 
-int kptr_restrict __read_mostly;
+int kptr_restrict __read_mostly = 2;
 
 /*
  * Show a '%p' thing.  A kernel extension is that the '%p' is followed
