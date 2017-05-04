518984f09abb5b4951d89956d7a651ba824b45f7
Daniel Micay <danielmicay@gmail.com>
2017-05-03 21:54:56 -0400

    add support for verifying page sanitization
    This is an extension to the sanitization feature in PaX for when
    sacricifing more performance for security is acceptable.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1775,6 +1775,12 @@ static void prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags
 
 	post_alloc_hook(page, order, gfp_flags);
 
+	if (IS_ENABLED(CONFIG_PAGE_SANITIZE_VERIFY)) {
+		unsigned long index = 1UL << order;
+		for (; index; --index)
+			verify_zero_highpage(page + index - 1);
+	}
+
 	if (!free_pages_prezeroed() && (gfp_flags & __GFP_ZERO))
 		for (i = 0; i < (1 << order); i++)
 			clear_highpage(page + i);
