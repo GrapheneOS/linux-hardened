b1841ebf400028f8cc459665c1835a541eb63494
Daniel Micay <danielmicay@gmail.com>
2017-05-07 00:28:23 -0400

    add __read_only for non-init related usage

--- include/linux/cache.h
+++ include/linux/cache.h
@@ -30,6 +30,8 @@
 #define __ro_after_init __attribute__((__section__(".data..ro_after_init")))
 #endif
 
+#define __read_only __ro_after_init
+
 #ifndef ____cacheline_aligned
 #define ____cacheline_aligned __attribute__((__aligned__(SMP_CACHE_BYTES)))
 #endif
