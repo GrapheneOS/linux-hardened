232ebb787be2df4e0030f99aed221a4e11ec6598
Daniel Micay <danielmicay@gmail.com>
2017-05-12 00:06:16 -0400

    mark __supported_pte_mask as __ro_after_init
    The call to x86_configure_nx in cpu_init isn't required, since it's
    already set up earlier.
    
    These changes were extracted from PaX where it's part of KERNEXEC as
    __read_only.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -543,7 +543,7 @@ static void __init pagetable_init(void)
 	permanent_kmaps_init(pgd_base);
 }
 
-pteval_t __supported_pte_mask __read_mostly = ~(_PAGE_NX | _PAGE_GLOBAL);
+pteval_t __supported_pte_mask __ro_after_init = ~(_PAGE_NX | _PAGE_GLOBAL);
 EXPORT_SYMBOL_GPL(__supported_pte_mask);
 
 /* user-defined highmem size */
