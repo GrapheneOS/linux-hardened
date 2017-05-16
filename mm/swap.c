3fab43a7d998dd01c5bc41aeec4e1d9a67584a4e
Daniel Micay <danielmicay@gmail.com>
2017-05-15 23:59:18 -0400

    add page destructor sanity check
    Taken from the public PaX patches.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -92,6 +92,13 @@ static void __put_compound_page(struct page *page)
 	if (!PageHuge(page))
 		__page_cache_release(page);
 	dtor = get_compound_page_dtor(page);
+	if (!PageHuge(page))
+		BUG_ON(dtor != free_compound_page
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+			&& dtor != free_transhuge_page
+#endif
+		);
+
 	(*dtor)(page);
 }
 
