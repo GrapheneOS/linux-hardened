8fb903f93a67be6c56be97650571f0043fbc2d03
Daniel Micay <danielmicay@gmail.com>
2017-05-16 18:26:10 -0400

    restrict device timing side channels
    Based on the public grsecurity patches.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3392,4 +3392,15 @@ static inline bool dir_relax_shared(struct inode *inode)
 extern bool path_noexec(const struct path *path);
 extern void inode_nohighmem(struct inode *inode);
 
+extern int device_sidechannel_restrict;
+
+static inline bool is_sidechannel_device(const struct inode *inode)
+{
+	umode_t mode;
+	if (!device_sidechannel_restrict)
+		return false;
+	mode = inode->i_mode;
+	return ((S_ISCHR(mode) || S_ISBLK(mode)) && (mode & (S_IROTH | S_IWOTH)));
+}
+
 #endif /* _LINUX_FS_H */
