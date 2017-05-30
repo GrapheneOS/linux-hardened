720fd6d0cb0384ee3ab60982272bd8eb7a46c56a
Daniel Micay <danielmicay@gmail.com>
2017-05-30 06:05:14 -0400

    mark dmi_system_id structs as const
    Partly extracted from PaX.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- drivers/platform/x86/intel_oaktrail.c
+++ drivers/platform/x86/intel_oaktrail.c
@@ -299,7 +299,7 @@ static int dmi_check_cb(const struct dmi_system_id *id)
 	return 0;
 }
 
-static struct dmi_system_id __initdata oaktrail_dmi_table[] = {
+static const struct dmi_system_id __initconst oaktrail_dmi_table[] = {
 	{
 		.ident = "OakTrail platform",
 		.matches = {
