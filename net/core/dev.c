fa60cf4f3407ab200680a4c56856fce794851950
Daniel Micay <danielmicay@gmail.com>
2017-07-04 01:41:11 -0400

    remove unused softirq_action callback parameter
    Extracted from PaX.

--- net/core/dev.c
+++ net/core/dev.c
@@ -4123,7 +4123,7 @@ int netif_rx_ni(struct sk_buff *skb)
 }
 EXPORT_SYMBOL(netif_rx_ni);
 
-static __latent_entropy void net_tx_action(struct softirq_action *h)
+static __latent_entropy void net_tx_action(void)
 {
 	struct softnet_data *sd = this_cpu_ptr(&softnet_data);
 
@@ -5666,7 +5666,7 @@ out_unlock:
 	return work;
 }
 
-static __latent_entropy void net_rx_action(struct softirq_action *h)
+static __latent_entropy void net_rx_action(void)
 {
 	struct softnet_data *sd = this_cpu_ptr(&softnet_data);
 	unsigned long time_limit = jiffies +
